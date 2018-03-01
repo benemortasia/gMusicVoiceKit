4d87f0867463fa0b4ab519d529e3c758df2f2905
Jordan Page <jpage628@gmail.com>
2018-03-01 12:59:56 -0500

    ASCII check; Improved Readability

--- SongManipulation/play_song_by_artist.py
+++ SongManipulation/play_song_by_artist.py
@@ -4,12 +4,12 @@ from gmusicapi import Musicmanager
 import re
 import os.path
 
-# import vlc
+import vlc
 
 gpm = Mobileclient()
 # Login with username, password, and MAC.
 # MAC helps Google Play Services identify the device
-gpm.login('jpage628@gmail.com', pw, Mobileclient.FROM_MAC_ADDRESS)
+gpm.login('jpage628@gmail.com', '_c0mplex', Mobileclient.FROM_MAC_ADDRESS)
 # User note: always 'escape' the backslashes, or we may get special characters
 # 'D:\testfolder' would translate the \t into a tab, for example:
 # 'D:    estfolder'
@@ -33,28 +33,42 @@ def play_song_by_artist(song="Raindrop", artist="Chopin"):
                 m = re.match(artist_pattern, song['artist'])
                 print m
                 print song_dict.pop()
+
                 if (re.match(song_pattern, song['title']) is not None and
                         re.match(artist_pattern, song['artist']) is not None):
                     print 'Song found!'
                     song_id = song['id']
                     filename, audio = mm.download_song(song_id)
 
+                    # get rid of non-ascii characters in file name
+                    filename = filename.encode('ascii', errors='ignore')
+
                     # check if song is already downloaded
+                    # path will look something like:
+                    # C:\\Users\\JP\Music\\Downloaded Music\\02 - Raindrop Prelude.mp3
+                    path = song_location + filename
                     try:
-                        print song_location + filename
-                        if os.path.isfile(song_location + filename):
+                        if os.path.isfile(path):
                             print 'Song is already downloaded...'
+                            print path
                             print 'Playing song.'
+                            # TODO
+                            # p = vlc.MediaPlayer(path)
+                            # p.play()
                             break
                         else:
-                            with open(song_location + filename, 'wb') as f:
+                            with open(path, 'wb') as f:
                                 f.write(audio)
+                            print 'Song has been added to: ' + path
+                            print 'Playing song.'
+                            # TODO
+                            # p = vlc.MediaPlayer(path)
+                            # p.play()
+                            break
                     except (OSError, IOError):
                         print 'An error has occurred.'
                         break
 
-                    # TODO
-                    # vlc.MediaPlayer.play(filename)
                 else:
                     print 'Song not found.'
         else:
