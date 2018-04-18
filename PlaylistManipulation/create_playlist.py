6da5ebcf22372af0c26cf3ad2fc43bbfa74681c7
Jordan Page <jpage628@gmail.com>
2018-04-18 17:18:57 -0400

    Update create_playlist.py

--- PlaylistManipulation/create_playlist.py
+++ PlaylistManipulation/create_playlist.py
@@ -1,18 +1,20 @@
 #!/usr/bin/env python3
+
 from gmusicapi import Mobileclient
 
-__author__ = "Jordan Page"
-__license__ = "MIT"
-__version__ = "1.0.0"
+__author__ = 'Jordan Page'
+__license__ = 'MIT'
+__version__ = '1.0.1'
 
 gpm = Mobileclient()
 
 # MAC helps Google Play Services identify the device
 # *** Change EXAMPLE and PASSWORD to your own Gmail login. ***
-gpm.login('EXAMPLE@gmail.com', 'PASSWORD', Mobileclient.FROM_MAC_ADDRESS)
+gpm.login('jpage628@gmail.com', '_c0mplex',
+          Mobileclient.FROM_MAC_ADDRESS)
 
 
-def create_playlist(playlist_name="Default Playlist Title"):
+def create_playlist(playlist_name):
 
     if Mobileclient.is_authenticated(gpm):
         # the following prints all playlist names
@@ -24,12 +26,13 @@ def create_playlist(playlist_name="Default Playlist Title"):
             # print(temp)
 
         return gpm.create_playlist(playlist_name)
-
     else:
         print("The Mobileclient is not authenticated.")
 
 
     Mobileclient.logout(gpm)
 
+# Useful if you want to test this function independently of cloudspeech
+# Replace 'Default' with a playlist name
 if __name__ == '__main__':
-    create_playlist()
+    create_playlist('Default')
